85dfbd03f87513884184adfb048aa2f83d87ab28
CnrHachi <44537516+ConnorHachi@users.noreply.github.com>
2019-11-20 23:27:02 +0800

    finished Eight_Puzzle.h

--- Eight_Puzzle.h
+++ Eight_Puzzle.h
@@ -5,7 +5,6 @@
 #ifndef ARTIFICIALINTELLIGENCEPROJECT_EIGHT_PUZZLE_H
 #define ARTIFICIALINTELLIGENCEPROJECT_EIGHT_PUZZLE_H
 
-
 #include <fstream>
 int ERROR = -1;
 
@@ -15,35 +14,37 @@ class Eight_Puzzle;
 
 class Eight_Puzzle{
 public:
-    int depth = 0;
+
     int flag = -1;
+
+    bool operator<(Eight_Puzzle p2);
+
+    bool operator==(Eight_Puzzle p2);
+
 
     Eight_Puzzle()= default;
     void ReadFile();
     bool isSolveable();
-//    static Eight_Puzzle move(Eight_Puzzle old_puzzle, int i);
     int getSpaceIndex();
-    int getf();
-
-    int Heuristic_Method(int method);
-    int Heuristic_Method1();
-    int Heuristic_Method2();
-    void Heuristic_Method3();
-    void Heuristic_Method4();
+    int getF();
+    int getH();
+    int getDepth();
+    void Heuristic_Method(int method);
     bool isGoal();
     void move(int i);
     static Eight_Puzzle move(Eight_Puzzle puzzle, int i);
     static Eight_Puzzle move(Eight_Puzzle puzzle,int i, int method);
     void setFather(Eight_Puzzle puzzle);
-    bool operator<(Eight_Puzzle p2);
-    bool operator==(Eight_Puzzle p2);
+
     int* getS0();
     Eight_Puzzle* getFather();
 
 private:
-    int h = 0;
     int Sg[9]{};
     int S0[9]{};
+    int S0_InverseNum = 0;
+    int h = 0;
+    int depth = 0;
     Eight_Puzzle *n = nullptr;
 
     int f = 0;
@@ -52,6 +53,7 @@ private:
 
 
 void Eight_Puzzle::ReadFile() {
+    Eight_Puzzle();
     ifstream Sg_File, S0_File;                //打开存储了初始状态与目标状态的TXT文件
     Sg_File.open ("../data/Sg.txt");
     S0_File.open ("../data/S0.txt");
@@ -73,26 +75,27 @@ void Eight_Puzzle::ReadFile() {
 }
 
 bool Eight_Puzzle::isSolveable() {
-    int *Sg_without0 = new int[8];
-    int *S0_without0 = new int[8];
+    int Sg_without0[8];
+    int S0_without0[8];
     int Sg_InverseNum = 0;
-    int S0_InverseNum = 0;
+    S0_InverseNum = 0;
 
+    int i1 = 0,m = 0;
     for (int i = 0; i < 9; ++i) {
         if(Sg[i] != 0){
-            *Sg_without0 = Sg[i];
-            Sg_without0++;
+            Sg_without0[i1] = Sg[i];
+            i1++;
         }
 
         if(S0[i] != 0){
-            *S0_without0 = Sg[i];
-            S0_without0++;
+            S0_without0[m] = S0[i];
+            m++;
         }
 
     }
 
     for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
+        for (int j = i + 1; j < 8; ++j) {
             if (Sg_without0[i] > Sg_without0[j])
                 Sg_InverseNum++;
             if (S0_without0[i] > S0_without0[j])
@@ -101,17 +104,6 @@ bool Eight_Puzzle::isSolveable() {
     }
 
     return Sg_InverseNum % 2 == S0_InverseNum % 2;
-}
-
-
-void Eight_Puzzle::Heuristic_Method4() {
-    int count = 0;
-    for(int i = 0; i < 9 && S0[i] != 0; i++){
-        for(int j = 0; S0[i]!=0 && j < 9; j++){
-            if(S0[i] == Sg[j])
-                count++;
-        }
-    }
 }
 
 bool Eight_Puzzle::isGoal() {
@@ -128,6 +120,10 @@ int Eight_Puzzle::getSpaceIndex() {
         if(S0[i] == 0)
             return i;
     return ERROR;
+}
+
+int Eight_Puzzle::getDepth() {
+    return depth;
 }
 
 void Eight_Puzzle::move(int i) {
@@ -154,7 +150,10 @@ Eight_Puzzle Eight_Puzzle::move(Eight_Puzzle puzzle,int i) {
 
 
 Eight_Puzzle Eight_Puzzle::move(Eight_Puzzle puzzle,int i,int method) {
-    Eight_Puzzle new_puzzle = puzzle;
+    Eight_Puzzle new_puzzle= puzzle;
+    new_puzzle.h = 0;
+    new_puzzle.f = 0;
+    new_puzzle.S0_InverseNum = 0;
     new_puzzle.move (i);
     new_puzzle.setFather (puzzle);
     new_puzzle.Heuristic_Method (method);
@@ -168,7 +167,9 @@ bool Eight_Puzzle::operator<(Eight_Puzzle p2) {
     return  f < p2.f;
 }
 
-int Eight_Puzzle::getf() {
+
+
+int Eight_Puzzle::getF() {
     return f;
 }
 
@@ -191,16 +192,19 @@ Eight_Puzzle* Eight_Puzzle::getFather() {
     return n;
 }
 
-int Eight_Puzzle::Heuristic_Method(int method) {
+void Eight_Puzzle::Heuristic_Method(int method) {
     switch(method){
+        default:
+            break;
         case 1:
             h = 0;
+            f = 0;
             for(int i = 0;i < 9;i++){
                 if(S0[i]!=0 && Sg[i] != S0[i] )
                     h++;
             }
             f = depth + h;
-            return f;
+            break;
 
         case 2:
             h = 0;
@@ -220,16 +224,62 @@ int Eight_Puzzle::Heuristic_Method(int method) {
                 }
             }
             f = depth + h;
-            return f;
+            break;
 
         case 3:
+            h = 0;
+            for(int i = 0,zero_S0 = 0; i < 9;i++){
+                if(S0[i] == 0){
+                    zero_S0 = 1;
+                    continue;
+                }
+
+                for (int j = 0,zero_Sg = 0; j < 9;j++) {
+                    if(Sg[j] == 0) {
+                        zero_Sg = 1;
+                        continue;
+                    }
+                    if(S0[i] == Sg[j] && zero_S0 == zero_Sg)
+                        h += abs (i - j);
+                    if(S0[i] == Sg[j] && zero_S0 != zero_Sg)
+                        h += abs (i - j) - 1;
+                }
+            }
+            f = h + depth;
             break;
+
         case 4:
-            break;
-        default:
+            h = 0;
+            S0_InverseNum = 0;
+            for(int i = 0;i < 9;i++){
+                if(S0[i]!=0 && Sg[i] != S0[i] )
+                    h++;
+            }
+            for(int i = 0,zero_S0 = 0; i < 9;i++){
+                if(S0[i] == 0){
+                    zero_S0 = 1;
+                    continue;
+                }
+
+                for (int j = 0,zero_Sg = 0; j < 9;j++) {
+                    if(Sg[j] == 0) {
+                        zero_Sg = 1;
+                        continue;
+                    }
+                    if(S0[i] == Sg[j] && zero_S0 == zero_Sg)
+                        S0_InverseNum += abs (i - j);
+                    if(S0[i] == Sg[j] && zero_S0 != zero_Sg)
+                        S0_InverseNum += abs (i - j) - 1;
+                }
+            }
+            h += 3 *S0_InverseNum;
+            f = depth + h;
             break;
     }
-    return 0;
+}
+
+int Eight_Puzzle::getH() {
+    return h;
 }
 
 
