f432e261804e3c79c226e24d159d7c3f5982e1a4
CnrHachi <44537516+ConnorHachi@users.noreply.github.com>
2019-11-20 23:55:29 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,53 +1,62 @@
 #include <iostream>
-#include <ctime>
-#include "Eight_Puzzle.h"
-#include "Width_First.cpp"
+
+#include "Breadth_First.cpp"
 #include "Best_First.cpp"
 #include "A_Star.cpp"
+
 using namespace std;
 
-void showMenu(){
-    int i;
-
-
-}
-
 int main() {
-
     int result = -1;
     int method = -1;
-    while(true){
-        int i;
+    Eight_Puzzle puzzle;
+    puzzle.ReadFile ();
+    if (!puzzle.isSolveable ()) {
+        cout << "您输入的八数码问题无解" << endl;
+        system ("PAUSE");
+        return 0;
+    } else
+        cout <<"您输入的八数码问题可解"<<endl;
+
+    while (true) {
         Eight_Puzzle puzzle;
         puzzle.ReadFile ();
-        if (!puzzle.isSolveable ()) {
-            cout << "您输入的八数码问题无解" << endl;
-        }
-        cout<<"********八数码问题求解程序*********"<<endl;
-        cout<<"1.盲目搜索中的宽度优先搜索算法"<<endl;
-        cout<<"2.启发式搜索中的全局择优搜索算法"<<endl;
-        cout<<"3.A*算法"<<endl;
-        cout<<"0.退出"<<endl;
-        cout<<"请输入执行的算法序号：";
+        int i;
+        cout << "********八数码问题求解程序*********" << endl;
+        cout << "1.盲目搜索中的宽度优先搜索算法" << endl;
+        cout << "2.启发式搜索中的全局择优搜索算法" << endl;
+        cout << "3.A*算法" << endl;
+        cout << "0.退出" << endl;
+        cout << "请输入执行的算法序号：";
         cin >> i;
-        if(i == 1){
-            result = Width_First (puzzle);
-        } else if(i == 2){
-            cout << "请输入使用的启发策略：";
-            cin >> method;
-            result = Best_First (puzzle,method);
-        }else if(i == 3){
-            result = A_Star (puzzle);
-        }else if( i == 0){
-            cout << "感谢使用！"<<endl;
-            break;
-        }else{
-            cout<<"输入格式错误，请重新输入。"<<endl;
+        switch (i) {
+            case 1:
+                result = Breadth_First (puzzle);
+                break;
+            case 2:
+                cout << "请输入使用的启发策略：";
+                cin >> method;
+                result = Best_First (puzzle, method);
+                break;
+            case 3:
+                result = A_Star (puzzle);
+                break;
+            case 0:
+                cout << "感谢使用！" << endl;
+                system ("PAUSE");
+                return 0;
+            default:
+                cout << "输入格式错误，请重新输入。" << endl;
+                break;
         }
-        if(result == EXIT_SUCCESS){
-            cout << "计算成功，结果可在Result.txt文件中查看。"<<endl;
-            cout << "感谢使用！"<<endl;
+        if (result == EXIT_SUCCESS) {
+            cout << "计算成功，结果可在Result.txt文件中查看。" << endl;
+            cout << "感谢使用！" << endl;
+        } else if (result == EXIT_FAILURE) {
+            cout << "计算失败";
         }
+        result = -1;
     }
 }
+
 
